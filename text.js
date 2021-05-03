f26b8800ca50c30022e060173261b6b384d9ae75
saurabhpatel18216 <81506377+saurabhpatel18216@users.noreply.github.com>
2021-05-03 14:02:32 +0530

    Update text.js

--- text.js
+++ text.js
@@ -12,15 +12,19 @@ const drawBoard = () => {
     let styleString = "";
     if (index < 3) {
       styleString += `border-bottom: 3px solid var(--purple);`;
+       styleString += `border-top: 3px solid var(--purple);`;
     }
     if (index % 3 === 0) {
       styleString += `border-right: 3px solid var(--purple);`;
+      styleString += `border-left: 3px solid var(--purple);`;
     }
     if (index % 3 === 2) {
       styleString += `border-left: 3px solid var(--purple);`;
+       styleString += `border-right: 3px solid var(--purple);`;
     }
     if (index > 5) {
       styleString += `border-top: 3px solid var(--purple);`;
+     styleString += `border-bottom: 3px solid var(--purple);`;
     }
     box.style = styleString;
 
